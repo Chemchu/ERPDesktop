c51d8bbf53686833e5d08eee243af9244ea52577
gus <gustavo_9_rj@hotmail.com>
2022-10-02 17:39:38 +0200

    First commit

---
+++ src-tauri/src/modelos/mod.rs
@@ -0,0 +1,13 @@
+pub struct Venta {
+    id: str,
+}
+
+pub struct ProductoVendido {
+    nombre: String,
+    cantidad_vendida: String,
+    ean: String,
+    precio_compra: String,
+    precio_venta: String,
+    precio_final: String,
+    dto: String,
+}
